ee6651421f8eb708f60ae8ad7c5305e8f33b496f
Yousef Ashraf <bosayousef981@gmail.com>
2022-05-03 13:21:25 +0200

    Fix navbar on resize

--- js/main.js
+++ js/main.js
@@ -1,40 +1,50 @@
-const menuBtn = document.querySelector("#menu-btn");
-const closeBtn = document.querySelector("#close-btn");
-const menu = document.querySelector(".nav__items");
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const menu = document.querySelector('.nav__items');
 
 // open nav menu
-menuBtn.addEventListener("click", () => {
-    menu.style.display = 'block';
-    menuBtn.style.display = 'none';
-    closeBtn.style.display = 'inline-block';
-})
+menuBtn.addEventListener('click', () => {
+	menu.style.display = 'block';
+	menuBtn.style.display = 'none';
+	closeBtn.style.display = 'inline-block';
+});
 
 // close nav menu
-closeBtn.addEventListener("click", () => {
-    menu.style.display = 'none';
-    menuBtn.style.display = 'inline-block';
-    closeBtn.style.display = 'none';
-})
-
+closeBtn.addEventListener('click', () => {
+	menu.style.display = 'none';
+	menuBtn.style.display = 'inline-block';
+	closeBtn.style.display = 'none';
+});
 
 // show/hide faqs
 const faqs = document.querySelectorAll('.faq');
 
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('open');
-        // change icon
-        const icon = faq.querySelector('.faq__icon i');
-        if(icon.className === 'uil uil-plus') {
-            icon.className = 'uil uil-minus';
-        } else {
-            icon.className = 'uil uil-plus';
-        }
-    })
-})
+faqs.forEach((faq) => {
+	faq.addEventListener('click', () => {
+		faq.classList.toggle('open');
+		// change icon
+		const icon = faq.querySelector('.faq__icon i');
+		if (icon.className === 'uil uil-plus') {
+			icon.className = 'uil uil-minus';
+		} else {
+			icon.className = 'uil uil-plus';
+		}
+	});
+});
 
-
+window.addEventListener('resize', () => {
+	if (window.innerWidth > 1024) {
+		menu.style.display = 'flex';
+		menuBtn.style.display = 'none';
+		closeBtn.style.display = 'none';
+	} else {
+		menu.style.display = 'none';
+		menuBtn.style.display = 'inline-block';
+	}
+});
 // add background style to navbar
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
-})
+	document
+		.querySelector('nav')
+		.classList.toggle('window-scroll', window.scrollY > 0);
+});
